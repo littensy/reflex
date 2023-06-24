b6faf9e9d5a4a7968e3e2829d50f2229a17038c1
Richard <richerd.rdc@gmail.com>
2023-06-24 00:15:44 -0700

    docs: fix paths

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/docs/quick-start">
+            to="/docs">
             Get Started →
           </Link>
         </div>
