f1aa00d89b412e7bd98acae4253675e7a1b0509b
Richard <richerd.rdc@gmail.com>
2023-06-28 13:23:51 -0700

    chore: fix typo

--- src/utils/testSelector.lua
+++ src/utils/testSelector.lua
@@ -2,8 +2,8 @@ local RunService = game:GetService("RunService")
 
 local IS_STUDIO = RunService:IsStudio()
 local TEST_WARNING = [[
-Reflex detected a selector function that returns different values for the same input!
-	This is could be because:
+Reflex detected a selector function that returns conflicting values for the same input!
+This is likely caused by one of the following:
 
 - The selector function is not memoized and should use 'createSelector'
 - The selector function is memoized but is not idempotent
