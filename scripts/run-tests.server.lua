4ce98dcf111b8a3e4be18f07f7fa4f0882393d51
Richard <richerd.rdc@gmail.com>
2023-07-16 12:47:33 -0700

    chore: fix transferred files

--- scripts/run-tests.server.lua
+++
@@ -1,8 +0,0 @@
---!nocheck
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
-local TestEZ = require(ReplicatedStorage.DevPackages.TestEZ)
-
-TestEZ.TestBootstrap:run({
-	ReplicatedStorage.Packages.Remo,
-})
